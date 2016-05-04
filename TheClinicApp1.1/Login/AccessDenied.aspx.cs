c188a26f25a861f202de3c49c25e5b933f4f7b94
tvm-thomson <thomson.varkey@thrithvam.me>
2016-05-04 16:06:36 +0530

    access denied admin view

--- TheClinicApp1.1/Login/AccessDenied.aspx.cs
+++ TheClinicApp1.1/Login/AccessDenied.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,11 +17,19 @@ namespace TheClinicApp1._1.Login
         public string From = "";
         protected void Page_Load(object sender, EventArgs e)
         {
+            List<string> RoleName = new List<string>();
+            DataTable dtRols = new DataTable();
             UA = (ClinicDAL.UserAuthendication)Session[Const.LoginSession];
             lblClinicName.Text = UA.Clinic;
             string Login = UA.userName;
-            RoleName = UA.GetRoleName(Login);
-            if (RoleName == Const.RoleAdministrator)
+            dtRols = UA.GetRoleName1(Login);
+            foreach (DataRow dr in dtRols.Rows)
+            {
+
+                RoleName.Add(dr["RoleName"].ToString());
+
+            }
+            if (RoleName.Contains(Const.RoleAdministrator))
             {
                 //this.hide.style.Add("display", "none");
                 this.admin.Style.Add("Visibility", "Visible");
