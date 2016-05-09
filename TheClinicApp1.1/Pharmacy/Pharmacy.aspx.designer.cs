6bd02376e798eaf429d8621e57fdc4a5468f8364
tvm-thomson <thomson.varkey@thrithvam.me>
2016-05-09 14:15:16 +0530

    leftbar view change

--- TheClinicApp1.1/Pharmacy/Pharmacy.aspx.designer.cs
+++ TheClinicApp1.1/Pharmacy/Pharmacy.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace TheClinicApp1._1.Pharmacy {
         protected global::System.Web.UI.WebControls.ImageButton LogoutButton;
         
         /// <summary>
+        /// btnSave control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSave;
+        
+        /// <summary>
         /// Errorbox control.
         /// </summary>
         /// <remarks>
