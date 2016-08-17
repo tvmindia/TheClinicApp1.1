281e3255e9bd22826b00324672ee135125ed1fde
tvm-albert <albert.thomson@thrithvam.ae>
2016-08-17 15:16:29 +0530

    new method doctor availabilty added

--- TheClinicApp1.1/ClinicDAL/Doctor.cs
+++ TheClinicApp1.1/ClinicDAL/Doctor.cs
@@ -704,6 +704,62 @@ namespace TheClinicApp1._1.ClinicDAL
         }
         #endregion CancelDoctorSchedule
 
+        #region GetDoctorAvailability
+        public DataSet GetDoctorAvailability()
+        {
+            if (ClinicID == "")
+            {
+                throw new Exception("ClinicID is Empty!!");
+            }
+            if (DoctorID == "")
+            {
+                throw new Exception("DoctorID is Empty!!");
+            }
+            if (DoctorAvailDate == "")
+            {
+                throw new Exception("DoctorAvailDate is Empty!!");
+            }
+          
+            dbConnection dcon = null;
+            SqlCommand cmd = null;
+            SqlDataAdapter sda = null;
+            DataSet ds = null;
+            try
+            {
+                dcon = new dbConnection();
+                dcon.GetDBConnection();
+                cmd = new SqlCommand();
+                sda = new SqlDataAdapter();
+                cmd.Connection = dcon.SQLCon;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[GetDoctorAvailablitydetails]";
+                cmd.Parameters.Add("@ClinicID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.ClinicID);
+                cmd.Parameters.Add("@DoctorID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(this.DoctorID);
+                cmd.Parameters.Add("@DoctorAvailDate", SqlDbType.Date).Value = DoctorAvailDate;
+                sda.SelectCommand = cmd;
+                ds = new DataSet();
+                sda.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                UA = (ClinicDAL.UserAuthendication)HttpContext.Current.Session[Const.LoginSession];
+                eObj.Description = ex.Message;
+                eObj.Module = Module;
+                eObj.UserID = UA.UserID;
+                eObj.Method = "GetDoctorAvailability";
+                eObj.InsertError();
+            }
+            finally
+            {
+                if (dcon.SQLCon != null)
+                {
+                    dcon.DisconectDB();
+                }
+            }
+            return ds;
+        }
+        #endregion GetDoctorAvailability
+
         #endregion Doctor Schedule methods
 
         #region Doctor Methods
