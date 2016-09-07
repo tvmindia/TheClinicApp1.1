3cdf71cf4900f9c301ba130da1433801a2ffcfa7
tvm-shamila <shamila.tp@thrithvam.me>
2016-09-07 15:36:33 +0530

    msg content changed

--- TheClinicApp1.1/Scripts/Common/Common.js
+++ TheClinicApp1.1/Scripts/Common/Common.js
@@ -35,8 +35,8 @@ function DisplayAlertMessages(lblclass, lblcaptn, lblmsg)
 
 
 
-var AppoinmentCancellationMessage = '<p>' + "Dear" + '<strong>' + " %PATIENT NAME%," + '</strong>' + '<br /><br />&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + "Your Appoinment with Dr." + '<strong>' + "%DOCTOR NAME%" + '</strong>' + "On" + '<strong>' + " %DATE%" + '</strong>' + "  At " + '<strong>' + " %TIME%" + '</strong>' + "  has been cancelled due to" + '<strong>' + " %REASON% " + '</strong>' + ". Please contact our office to schedule a new appoinment." + '<br /><br />' + '</p>';
+var AppoinmentCancellationMessage = '<p>' + "Dear" + '<strong>' + " %PATIENT NAME%," + '</strong>' + '<br /><br />&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + "Your Appoinment with Dr." + '<strong>' + "%DOCTOR NAME%" + '</strong>' + "On" + '<strong>' + " %DATE%" + '</strong>' + "  At " + '<strong>' + " %TIME%" + '</strong>' + "  has been cancelled due to" + '<strong>' + " %REASON% " + '</strong>' +'<br />' + '</p>';
 
-var AppoinmentCancellationMessageWithoutHtml =  "Dear %PATIENT NAME%, Your Appoinment with Dr.%DOCTOR NAME% On %DATE% At %TIME% has been cancelled due to %REASON%.Please contact our office to schedule a new appoinment.";
+var AppoinmentCancellationMessageWithoutHtml =  "Dear %PATIENT NAME%, Your Appoinment with Dr.%DOCTOR NAME% On %DATE% At %TIME% has been cancelled due to %REASON%.";
 
 var CancelReason = "Some inconvenience";
