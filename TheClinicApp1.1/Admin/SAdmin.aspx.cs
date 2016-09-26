13b710dad1f2c8176d050544363ac2c3a40ff401
tvm-shamila <shamila.tp@thrithvam.me>
2016-09-26 17:28:06 +0530

    proper regions in sadmin

--- TheClinicApp1.1/Admin/SAdmin.aspx.cs
+++ TheClinicApp1.1/Admin/SAdmin.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+#region Included Namespaces
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,129 +12,41 @@ using System.Data;
 using System.IO;
 using System.Web.Services;
 using System.Web.Script.Serialization;
+#endregion Included Namespaces
 
 namespace TheClinicApp1._1.Admin
 {
     public partial class SAdmin : System.Web.UI.Page
     {
+        #region Global Variables
+
         UIClasses.Const Const = new UIClasses.Const();
         private static int PageSize = 8;
         ClinicDAL.Master MasterObj = new ClinicDAL.Master();
         ClinicDAL.UserAuthendication UA;
-        protected void Page_Load(object sender, EventArgs e)
+
+        #endregion Global Variables
+
+        #region Methods
+
+        #region User View Search Paging
+
+        [WebMethod]
+        public static string ViewAndFilterClinic(string searchTerm, int pageIndex)
         {
-            UA = (ClinicDAL.UserAuthendication)Session[Const.LoginSession];
-            BindDummyRow();
-           if(!IsPostBack)
-           {
-               BindDropDownGroupforDoc();
-           } 
-            
-        }
-
-        protected void Logout_ServerClick(object sender, EventArgs e)
-        {
-            Session.Remove(Const.LoginSession);
-            Response.Redirect("../Default.aspx");
-        }
-
-        protected void LogoutButton_Click(object sender, ImageClickEventArgs e)
-        {
-            Session.Remove(Const.LoginSession);
-            Response.Redirect("../Default.aspx");
-        }
-
-        protected void btnSave_Click(object sender, EventArgs e)
-         {
-            //string message = "";
-             try
-             {
-                 if (hdnGroupselect.Value == "Update")
-                 {
-                     MasterObj.ClinicID = Guid.Parse(hdnClinicID.Value);
-                     MasterObj.ClinicName = txtClinicName.Value;
-                     MasterObj.ClinicLocation = txtLocation.Value;
-                     MasterObj.ClinicPhone = txtPhone.Value;
-                     MasterObj.ClinicAddress = txtAddress.Value;
-                     MasterObj.updatedBy = UA.userName;
-                     if (fluplogo.HasFile)
-                     {
-                         byte[] ImageByteArray = null;
-                         ImageByteArray = ConvertImageToByteArray(fluplogo);
-                         MasterObj.Logo = ImageByteArray;
-
-                     }
-                     if (fluplogosmall.HasFile)
-                     {
-                         byte[] ImageByteArray = null;
-                         ImageByteArray = ConvertImageToByteArray(fluplogosmall);
-                         MasterObj.Logosmall = ImageByteArray;
-
-                     }
-                     MasterObj.UpdateClinic();
-
-                 }
-                 else
-                 {
-                     if (hdnGroupselect.Value == "New")
-                     {
-                         MasterObj.GroupName = txtGroupName.Value;
-                         if (fluplogo.HasFile)
-                         {
-                             byte[] ImageByteArray = null;
-                             ImageByteArray = ConvertImageToByteArray(fluplogo);
-                             MasterObj.Logo = ImageByteArray;
-
-                         }
-
-                         MasterObj.Createdby = UA.userName;
-                         MasterObj.InsertGroups();
-                     }
-                     if (hdnGroupselect.Value == "Exist")
-                     {
-                         MasterObj.GroupID = Guid.Parse(ddlGroup.SelectedValue);
-                     }
-
-                     MasterObj.ClinicName = txtClinicName.Value;
-                     MasterObj.ClinicAddress = txtAddress.Value;
-                     MasterObj.ClinicLocation = txtLocation.Value;
-                     MasterObj.ClinicPhone = txtPhone.Value;
-                     MasterObj.Createdby = UA.userName;
-                     if (fluplogo.HasFile)
-                     {
-                         byte[] ImageByteArray = null;
-                         ImageByteArray = ConvertImageToByteArray(fluplogo);
-                         MasterObj.Logo = ImageByteArray;
-
-                     }
-                     MasterObj.InsertClinics();
-                     hdnClinicID.Value = MasterObj.ClinicID.ToString();
-                     foreach (ListItem item in lstFruits.Items)
-                     {
-                         if (item.Selected)
-                         {
-                             //message += item.Text;
-                             MasterObj.RoleName = item.Text;
-                             MasterObj.createdBy = UA.userName;
-                             MasterObj.InsertRole();
-                             
-                         }
-                     }
-                     BindDropDownGroupforDoc();
-                 }
-                 
+            ClinicDAL.UserAuthendication UA;
+            UIClasses.Const Const = new UIClasses.Const();
 
 
-                 
+            UA = (ClinicDAL.UserAuthendication)HttpContext.Current.Session[Const.LoginSession];
+            Clinic ClinicObj = new Clinic();
+            //usrObj.ClinicID = UA.ClinicID;
+            var xml = ClinicObj.ViewAndFilterClinic(searchTerm, pageIndex, PageSize);
+            return xml;
 
-             }
-             catch
-             {
-
-             }
-           
 
         }
+        #endregion User View Search Paging
 
         #region Bind Dummy Row
 
@@ -157,6 +71,7 @@ namespace TheClinicApp1._1.Admin
         }
 
         #endregion Bind Dummy Row
+
         #region BindUserDetailsOnEditClick
         /// <summary>
         /// To get specific order details by orderid for the editing purpose
@@ -201,6 +116,9 @@ namespace TheClinicApp1._1.Admin
             return jsonResult; //Converting to Json
         }
         #endregion BindUserDetailsOnEditClick
+
+        #region ConvertImageToByteArray
+
         private byte[] ConvertImageToByteArray(FileUpload fuImage)
         {
             byte[] ImageByteArray;
@@ -215,6 +133,10 @@ namespace TheClinicApp1._1.Admin
                 return null;
             }
         }
+
+        #endregion ConvertImageToByteArray
+
+        #region BindDropDownGroupforDoc
         public void BindDropDownGroupforDoc()
         {
 
@@ -222,33 +144,146 @@ namespace TheClinicApp1._1.Admin
             dt = MasterObj.BindGroupName();
             ddlGroup.DataSource = dt;
             ddlGroup.DataTextField = "Name";
-            ddlGroup.DataValueField= "GroupID";
+            ddlGroup.DataValueField = "GroupID";
             ddlGroup.DataBind();
             ddlGroup.Items.Insert(0, new ListItem("--Select Group--", "-1"));
-        } 
-   
-     #region User View Search Paging
-      
-        [WebMethod]
-    public static string ViewAndFilterClinic(string searchTerm, int pageIndex)
+        }
+
+        #endregion BindDropDownGroupforDoc
+
+        #endregion Methods
+
+        #region Events
+
+        #region Page Load
+        protected void Page_Load(object sender, EventArgs e)
         {
-            ClinicDAL.UserAuthendication UA;
-            UIClasses.Const Const = new UIClasses.Const();
-    
+            UA = (ClinicDAL.UserAuthendication)Session[Const.LoginSession];
+            BindDummyRow();
+           if(!IsPostBack)
+           {
+               BindDropDownGroupforDoc();
+           } 
             
-            UA = (ClinicDAL.UserAuthendication)HttpContext.Current.Session[Const.LoginSession];
-            Clinic ClinicObj = new Clinic();
-                //usrObj.ClinicID = UA.ClinicID;
-            var xml = ClinicObj.ViewAndFilterClinic(searchTerm, pageIndex, PageSize);
-                return xml;
-                        
+        }
+
+        #endregion Page Load
+
+        #region Logout Click
+        protected void Logout_ServerClick(object sender, EventArgs e)
+        {
+            Session.Remove(Const.LoginSession);
+            Response.Redirect("../Default.aspx");
+        }
+
+        protected void LogoutButton_Click(object sender, ImageClickEventArgs e)
+        {
+            Session.Remove(Const.LoginSession);
+            Response.Redirect("../Default.aspx");
+        }
+
+        #endregion  Logout Click
+
+        #region Save Click
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            //string message = "";
+            try
+            {
+                if (hdnGroupselect.Value == "Update")
+                {
+                    MasterObj.ClinicID = Guid.Parse(hdnClinicID.Value);
+                    MasterObj.ClinicName = txtClinicName.Value;
+                    MasterObj.ClinicLocation = txtLocation.Value;
+                    MasterObj.ClinicPhone = txtPhone.Value;
+                    MasterObj.ClinicAddress = txtAddress.Value;
+                    MasterObj.updatedBy = UA.userName;
+                    if (fluplogo.HasFile)
+                    {
+                        byte[] ImageByteArray = null;
+                        ImageByteArray = ConvertImageToByteArray(fluplogo);
+                        MasterObj.Logo = ImageByteArray;
+
+                    }
+                    if (fluplogosmall.HasFile)
+                    {
+                        byte[] ImageByteArray = null;
+                        ImageByteArray = ConvertImageToByteArray(fluplogosmall);
+                        MasterObj.Logosmall = ImageByteArray;
+
+                    }
+                    MasterObj.UpdateClinic();
+
+                }
+                else
+                {
+                    if (hdnGroupselect.Value == "New")
+                    {
+                        MasterObj.GroupName = txtGroupName.Value;
+                        if (fluplogo.HasFile)
+                        {
+                            byte[] ImageByteArray = null;
+                            ImageByteArray = ConvertImageToByteArray(fluplogo);
+                            MasterObj.Logo = ImageByteArray;
+
+                        }
+
+                        MasterObj.Createdby = UA.userName;
+                        MasterObj.InsertGroups();
+                    }
+                    if (hdnGroupselect.Value == "Exist")
+                    {
+                        MasterObj.GroupID = Guid.Parse(ddlGroup.SelectedValue);
+                    }
+
+                    MasterObj.ClinicName = txtClinicName.Value;
+                    MasterObj.ClinicAddress = txtAddress.Value;
+                    MasterObj.ClinicLocation = txtLocation.Value;
+                    MasterObj.ClinicPhone = txtPhone.Value;
+                    MasterObj.Createdby = UA.userName;
+                    if (fluplogo.HasFile)
+                    {
+                        byte[] ImageByteArray = null;
+                        ImageByteArray = ConvertImageToByteArray(fluplogo);
+                        MasterObj.Logo = ImageByteArray;
+
+                    }
+                    MasterObj.InsertClinics();
+                    hdnClinicID.Value = MasterObj.ClinicID.ToString();
+                    foreach (ListItem item in lstFruits.Items)
+                    {
+                        if (item.Selected)
+                        {
+                            //message += item.Text;
+                            MasterObj.RoleName = item.Text;
+                            MasterObj.createdBy = UA.userName;
+                            MasterObj.InsertRole();
+
+                        }
+                    }
+                    BindDropDownGroupforDoc();
+                }
+
+            }
+            catch
+            {
+
+            }
+
 
         }
-        #endregion User View Search Paging
 
+        #endregion Save Save Click
+
+        #region Update Click
         protected void ImgBtnUpdate_Click(object sender, ImageClickEventArgs e)
         {
 
         }
-}
+
+        #endregion Update Click
+
+        #endregion Events
+    }
 }
